a7eac97c6882533b6323cf0be7b353e8f9a00330
Chaosotter <chaosotter76@gmail.com>
2020-06-12 01:03:54 +0000

    Adds a utility to convert Scott Adams adventures to proto format.

---
+++ cmd/convert_scott/convert_scott.go
@@ -0,0 +1,42 @@
+// convert_scott is a utility for converting a Scott Adams adventure file from
+// the TRS-80 format supported by the ScottFree interpreter to the proto-based
+// version used by this system.
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/chaosotter/golang-adventures/internal/scott/game"
+)
+
+var (
+	inPath  = flag.String("in", "", "Path to the input game file in ScottFree (TRS-80) format.")
+	outPath = flag.String("out", "", "Path to the output game file in proto format.")
+)
+
+func main() {
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*inPath)
+	if err != nil {
+		log.Fatalf("Could not read %q: %v", *inPath, err)
+	}
+
+	g, err := game.New(data)
+	if err != nil {
+		log.Fatalf("Could not parse %q: %v", *inPath, err)
+	}
+
+	wire, err := proto.Marshal(g.Initial)
+	if err != nil {
+		log.Fatalf("Could not marshal proto: %v", err)
+	}
+
+	if err := ioutil.WriteFile(*outPath, wire, 0664); err != nil {
+		log.Fatalf("Could not write %q: %v", *outPath, err)
+	}
+}
